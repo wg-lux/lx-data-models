b8d63b8ff8bd6e3bd8ce5a5ae770d9320e4f4b4f
maxhild <maxhild10@gmail.com>
2026-03-24 16:13:03 +0100

    Import all modules

--- lx_dtypes/django/models.py
+++ lx_dtypes/django/models.py
@@ -1,7 +1,93 @@
 from lx_dtypes.models.knowledge_base.citation.CitationDjango import CitationDjango
+from lx_dtypes.models.knowledge_base.classification._ClassificationDjango import (
+    ClassificationDjango,
+)
+from lx_dtypes.models.knowledge_base.classification._ClassificationTypeDjango import (
+    ClassificationTypeDjango,
+)
 from lx_dtypes.models.knowledge_base.classification_choice.ClassificationChoiceDjango import (
     ClassificationChoiceDjango,
 )
+from lx_dtypes.models.knowledge_base.classification_choice_descriptor.ClassificationChoiceDescriptorDjango import (
+    ClassificationChoiceDescriptorDjango,
+)
+from lx_dtypes.models.knowledge_base.examination.ExaminationDjango import (
+    ExaminationDjango,
+)
+from lx_dtypes.models.knowledge_base.examination.ExaminationTypeDjango import (
+    ExaminationTypeDjango,
+)
+from lx_dtypes.models.knowledge_base.finding._FindingDjango import FindingDjango
+from lx_dtypes.models.knowledge_base.finding._FindingTypeDjango import (
+    FindingTypeDjango,
+)
+from lx_dtypes.models.knowledge_base.indication.IndicationDjango import (
+    IndicationDjango,
+)
+from lx_dtypes.models.knowledge_base.indication.IndicationTypeDjango import (
+    IndicationTypeDjango,
+)
+from lx_dtypes.models.knowledge_base.information_source.InformationSourceDjango import (
+    InformationSourceDjango,
+)
+from lx_dtypes.models.knowledge_base.information_source.InformationSourceTypeDjango import (
+    InformationSourceTypeDjango,
+)
+from lx_dtypes.models.knowledge_base.intervention.InterventionDjango import (
+    InterventionDjango,
+)
+from lx_dtypes.models.knowledge_base.intervention.InterventionTypeDjango import (
+    InterventionTypeDjango,
+)
+from lx_dtypes.models.knowledge_base.unit.UnitDjango import UnitDjango
+from lx_dtypes.models.knowledge_base.unit.UnitTypeDjango import UnitTypeDjango
 from lx_dtypes.models.ledger.center.Django import CenterDjango
+from lx_dtypes.models.ledger.examiner.Django import ExaminerDjango
+from lx_dtypes.models.ledger.p_examination.Django import PExaminationDjango
+from lx_dtypes.models.ledger.p_finding.Django import PFindingDjango
+from lx_dtypes.models.ledger.p_finding_classification_choice.Django import (
+    PFindingClassificationChoiceDjango,
+)
+from lx_dtypes.models.ledger.p_finding_classification_choice_descriptor.Django import (
+    PFindingClassificationChoiceDescriptorDjango,
+)
+from lx_dtypes.models.ledger.p_finding_classifications.Django import (
+    PFindingClassificationsDjango,
+)
+from lx_dtypes.models.ledger.p_indication.Django import PIndicationDjango
+from lx_dtypes.models.ledger.p_intervention.Django import PFindingInterventionDjango
+from lx_dtypes.models.ledger.p_interventions.Django import (
+    PFindingInterventionsDjango,
+)
+from lx_dtypes.models.ledger.patient.Django import PatientDjango
 
-__all__ = ["CitationDjango", "ClassificationChoiceDjango", "CenterDjango"]
+__all__ = [
+    "CenterDjango",
+    "CitationDjango",
+    "ClassificationChoiceDescriptorDjango",
+    "ClassificationChoiceDjango",
+    "ClassificationDjango",
+    "ClassificationTypeDjango",
+    "ExaminationDjango",
+    "ExaminationTypeDjango",
+    "ExaminerDjango",
+    "FindingDjango",
+    "FindingTypeDjango",
+    "IndicationDjango",
+    "IndicationTypeDjango",
+    "InformationSourceDjango",
+    "InformationSourceTypeDjango",
+    "InterventionDjango",
+    "InterventionTypeDjango",
+    "PExaminationDjango",
+    "PFindingClassificationChoiceDescriptorDjango",
+    "PFindingClassificationChoiceDjango",
+    "PFindingClassificationsDjango",
+    "PFindingDjango",
+    "PFindingInterventionDjango",
+    "PFindingInterventionsDjango",
+    "PatientDjango",
+    "PIndicationDjango",
+    "UnitDjango",
+    "UnitTypeDjango",
+]
